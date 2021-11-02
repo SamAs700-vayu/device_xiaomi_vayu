f9d14241726204185904378b21f0c6c854993aca
johnmart19 <johnivan19999@gmail.com>
2021-11-02 23:51:13 +0200

    vayu: EvolutionOS Bringup

--- evolution_vayu.mk
+++ evolution_vayu.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018-2021 ArrowOS
+# Copyright (C) 2021 EvolutionOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,14 +11,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-# Inherit common ArrowOS configurations
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit common EvolutionOS configurations
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
-PRODUCT_NAME := arrow_vayu
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+
+PRODUCT_NAME := evolution_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := Poco X3 Pro
 PRODUCT_MANUFACTURER := Xiaomi
+
+# Gapps
+USE_GAPPS := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
